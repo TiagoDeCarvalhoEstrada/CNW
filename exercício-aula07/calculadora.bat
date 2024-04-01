e88a139d815f076b2ea2f543dc503511496609a6
Tiago De Carvalho Estrada <124801828+TiagoDeCarvalhoEstrada@users.noreply.github.com>
2024-04-01 12:41:45 -0300

    Delete exercício-aula07 directory

--- exercício-aula07/calculadora.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-::variáveis
-set /p ope=DIGITE A OPERACAO:
-set /p num1=DIGITE UM NUMERO:
-set /p num2=DIGITE OUTRO NUMERO:
-set /a opeprin=%num1%%ope%%num2%
-echo o resultado e: %opeprin%
-pause >nul
