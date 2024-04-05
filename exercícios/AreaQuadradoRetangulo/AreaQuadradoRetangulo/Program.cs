3eeb4098e6832d99b1e311fb8480226cbf8532a5
Tiago De Carvalho Estrada <124801828+TiagoDeCarvalhoEstrada@users.noreply.github.com>
2024-04-05 12:47:09 -0300

    Add files via upload

---
+++ exercícios/AreaQuadradoRetangulo/AreaQuadradoRetangulo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AreaQuadradoRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculo de área, quadrado ou retângulo!!");
+            Area area = new Area();
+            Console.WriteLine("Digite o valor da Base:");
+            area.Base = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o valor da Altura:");
+            area.Altura = Convert.ToInt32(Console.ReadLine());
+
+          //Console.WriteLine("Area: " + area.Calcular());
+            Console.WriteLine("Area: " + area.Calcular2());
+
+            area.MostrarDetalhes();
+        }
+    }
+}
---
+++ exercícios/Exercício2/Exercício2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Formats.Asn1;
+
+namespace Exercício2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Area area = new Area();
+            Console.WriteLine("Digite a base:");
+            area.Base = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite a altura:");
+            area.Altura = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.WriteLine("Escolha: \n1-QUADRADO \n2-RETÂNGULO \n3-TRIÂNGULO");
+            int opcao = Convert.ToInt32(Console.ReadLine());
+            area.Escolha(opcao);
+
+
+
+
+
+
+        }
+    }
+}
